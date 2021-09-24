829ea6e16fa7eef25762be57299043f83116d8cb
pointfeev <zehmeen@gmail.com>
2021-09-24 15:28:57 -0400

    Add better exception handler

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Debugger")]
+[assembly: AssemblyTitle("SortedIncome")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Debugger")]
+[assembly: AssemblyProduct("SortedIncome")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
--- Utilities/OutputUtils.cs
+++ Utilities/OutputUtils.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents;
-using TaleWorlds.Core;
-using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 
@@ -14,8 +13,19 @@ namespace SortedIncome
 
         public static void DoOutputForException(Exception e)
         {
+            string[] stackTrace = e.StackTrace.Split('\n');
+            string location = "STACK TRACE\n";
+            for (int i = 0; i <= 3; i++)
+            {
+                string line = stackTrace.ElementAtOrValue(i, null);
+                if (!(line is null))
+                {
+                    location += "\n    " + line.Substring(line.IndexOf("at"));
+                    break;
+                }
+            }
             string[] messageLines = e.Message.Split('\n');
-            string message = string.Empty;
+            string message = "MESSAGE\n";
             for (int i = 0; i <= messageLines.Length; i++)
             {
                 string line = messageLines.ElementAtOrValue(i, null);
@@ -24,22 +34,15 @@ namespace SortedIncome
                     message += "\n    " + messageLines[i];
                 }
             }
-            string[] stackTrace = e.StackTrace.Split('\n');
-            string location = string.Empty;
-            for (int i = 0; i <= 3; i++)
-            {
-                string line = stackTrace.ElementAtOrValue(i, null);
-                if (!(line is null) && !line.Contains("ThrowHelper"))
-                {
-                    location = line.Substring(line.IndexOf("at"));
-                    break;
-                }
-            }
-            string output = "SortedIncome encountered an exception " + location + message;
+            string output = location + "\n\n" + message +
+                "\n\nBUG REPORTING: The easiest way to report this error is to snap an image of this message box with Snipping Tool or Lightshot," +
+                "upload the image to imgur.com, and paste the link to the image in a new bug report on Nexus Mods (along with any helpful details)." +
+                "\n\nNOTE: This is not a game crash; press OK to continue playing.";
             if (!outputs.Contains(output))
             {
                 outputs.Add(output);
-                InformationManager.DisplayMessage(new InformationMessage(output, Colors.Red, "SortedIncome"));
+                MessageBox.Show(output, caption: "Sorted Income encountered an exception", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+                //InformationManager.DisplayMessage(new InformationMessage(output, Colors.Red, "SortedIncome"));
             }
         }
 
