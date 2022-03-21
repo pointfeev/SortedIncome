061f5af62986e88513eaacf9fdf0ff19a814201d
pointfeev <zehmeen@gmail.com>
2022-03-21 14:16:40 -0400

    directive changes for e1.7.2

--- Sorting.cs
+++ Sorting.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Reflection;
 
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.Settlements.Buildings;
 using TaleWorlds.Core;
 using TaleWorlds.InputSystem;
 using TaleWorlds.Library;
--- Utilities/OutputUtils.cs
+++ Utilities/OutputUtils.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 
-using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 
