d5ccd11f1f66d03086af0161c85e20e4a4a0f8e5
pointfeev <zehmeen@gmail.com>
2023-01-20 01:28:33 -0500

    xml automation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SortedIncome")]
+[assembly: AssemblyProduct("Aggregated Income")]
 [assembly: AssemblyCopyright("2021, pointfeev (https://github.com/pointfeev)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("4.1.2.0")]
