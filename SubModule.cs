feca16e4a4a95a9e47be8554c18531bea0d5c37c
pointfeev <zehmeen@gmail.com>
2023-04-05 14:44:12 -0400

    Update SubModule.cs

--- SubModule.cs
+++ SubModule.cs
@@ -20,65 +20,75 @@ public class SubModule : MBSubModuleBase
 
     protected override void OnBeforeInitialModuleScreenSetAsRoot()
     {
-        if (initialized)
-            return;
-        initialized = true;
-        List<string> failures = new();
-        Sorting.Value = AccessTools.Field(typeof(TextObject), "Value");
-        if (Sorting.Value == null)
-            failures.Add("Failed to get TextObject.Value field");
-        else
+        base.OnBeforeInitialModuleScreenSetAsRoot();
+        try
         {
-            IEnumerable<FieldInfo> fields = AccessTools.GetDeclaredFields(typeof(DefaultClanFinanceModel));
-            foreach (FieldInfo field in fields)
-                if (field.FieldType == typeof(TextObject))
-                    Sorting.ModelTextValues.Add(field.Name.Trim(' ', '_').Replace("Str", ""), (string)Sorting.Value.GetValue((TextObject)field.GetValue(null)));
-            if (Sorting.ModelTextValues.Count == 0)
-                failures.Add("Failed to get any DefaultClanFinanceModel TextObject fields");
+            if (initialized)
+                return;
+            initialized = true;
+            List<string> failures = new();
+            Sorting.Value = AccessTools.Field(typeof(TextObject), "Value");
+            if (Sorting.Value == null)
+                failures.Add("Failed to get TextObject.Value field");
+            else
+            {
+                IEnumerable<FieldInfo> fields = AccessTools.GetDeclaredFields(typeof(DefaultClanFinanceModel));
+                foreach (FieldInfo field in fields)
+                    if (field.FieldType == typeof(TextObject))
+                        Sorting.ModelTextValues.Add(field.Name.Trim(' ', '_').Replace("Str", ""),
+                            (string)Sorting.Value.GetValue((TextObject)field.GetValue(null)));
+                if (Sorting.ModelTextValues.Count == 0)
+                    failures.Add("Failed to get any DefaultClanFinanceModel TextObject fields");
+            }
+            Type statExplainer = AccessTools.TypeByName(typeof(ExplainedNumber).FullName + "+StatExplainer");
+            if (statExplainer == null)
+                failures.Add("Failed to get ExplainedNumber+StatExplainer type");
+            else
+            {
+                Sorting.AddLine = AccessTools.Method(statExplainer, "AddLine");
+                if (Sorting.AddLine == null)
+                    failures.Add("Failed to get StatExplainer.AddLine method");
+                Type operationType = AccessTools.TypeByName(statExplainer.FullName + "+OperationType");
+                if (operationType != null)
+                    try
+                    {
+                        Sorting.OperationType = Enum.ToObject(operationType, 1);
+                    }
+                    catch
+                    {
+                        // ignore
+                    }
+                if (Sorting.OperationType == null)
+                    failures.Add("Failed to get StatExplainer+OperationType enum");
+            }
+            if (failures.Count > 0)
+            {
+                OutputUtils.DoOutput(string.Join("\n", failures), OutputType.Initialization);
+                return;
+            }
+            Harmony harmony = new("pointfeev.sortedincome");
+            HarmonyMethod get = new(typeof(Sorting), nameof(Sorting.GetTooltip));
+            _ = harmony.Patch(AccessTools.Method(typeof(CampaignUIHelper), nameof(CampaignUIHelper.GetTooltipForAccumulatingProperty)), finalizer: get);
+            _ = harmony.Patch(AccessTools.Method(typeof(CampaignUIHelper), nameof(CampaignUIHelper.GetTooltipForAccumulatingPropertyWithResult)),
+                finalizer: get);
+            HarmonyMethod tick = new(typeof(Sorting), nameof(Sorting.TickTooltip));
+            _ = harmony.Patch(AccessTools.Method(typeof(PropertyBasedTooltipVM), nameof(PropertyBasedTooltipVM.Tick)), finalizer: tick);
+            HarmonyMethod show = new(typeof(Sorting), nameof(Sorting.ShowTooltip));
+            _ = harmony.Patch(AccessTools.Method(typeof(PropertyBasedTooltipVM), nameof(PropertyBasedTooltipVM.OnShowTooltip)), finalizer: show);
+            HarmonyMethod begin = new(typeof(Sorting), nameof(Sorting.BeginTooltip));
+            _ = harmony.Patch(AccessTools.Method(typeof(BasicTooltipViewModel), nameof(BasicTooltipViewModel.ExecuteBeginHint)), finalizer: begin);
+            HarmonyMethod add = new(typeof(Sorting), nameof(Sorting.AddTooltip));
+            _ = harmony.Patch(AccessTools.Method(typeof(ExplainedNumber), nameof(ExplainedNumber.Add)), add);
+            if (ModuleHelper.GetModuleInfo("Native").Version < ApplicationVersion.FromString("v1.1.0"))
+                return;
+            HarmonyMethod include = new(typeof(Sorting), nameof(Sorting.IncludeDetails));
+            _ = harmony.Patch(AccessTools.Method(typeof(DefaultClanFinanceModel), nameof(DefaultClanFinanceModel.CalculateClanGoldChange)), include);
+            _ = harmony.Patch(AccessTools.Method(typeof(DefaultClanFinanceModel), nameof(DefaultClanFinanceModel.CalculateClanIncome)), include);
+            _ = harmony.Patch(AccessTools.Method(typeof(DefaultClanFinanceModel), nameof(DefaultClanFinanceModel.CalculateClanExpenses)), include);
         }
-        Type statExplainer = AccessTools.TypeByName(typeof(ExplainedNumber).FullName + "+StatExplainer");
-        if (statExplainer == null)
-            failures.Add("Failed to get ExplainedNumber+StatExplainer type");
-        else
+        catch (Exception e)
         {
-            Sorting.AddLine = AccessTools.Method(statExplainer, "AddLine");
-            if (Sorting.AddLine == null)
-                failures.Add("Failed to get StatExplainer.AddLine method");
-            Type operationType = AccessTools.TypeByName(statExplainer.FullName + "+OperationType");
-            if (operationType != null)
-                try
-                {
-                    Sorting.OperationType = Enum.ToObject(operationType, 1);
-                }
-                catch
-                {
-                    // ignore
-                }
-            if (Sorting.OperationType == null)
-                failures.Add("Failed to get StatExplainer+OperationType enum");
+            OutputUtils.DoOutputForException(e);
         }
-        if (failures.Count > 0)
-        {
-            OutputUtils.DoOutput(string.Join("\n", failures), OutputType.Initialization);
-            return;
-        }
-        Harmony harmony = new("pointfeev.sortedincome");
-        HarmonyMethod get = new(typeof(Sorting), nameof(Sorting.GetTooltip));
-        _ = harmony.Patch(AccessTools.Method(typeof(CampaignUIHelper), nameof(CampaignUIHelper.GetTooltipForAccumulatingProperty)), finalizer: get);
-        _ = harmony.Patch(AccessTools.Method(typeof(CampaignUIHelper), nameof(CampaignUIHelper.GetTooltipForAccumulatingPropertyWithResult)), finalizer: get);
-        HarmonyMethod tick = new(typeof(Sorting), nameof(Sorting.TickTooltip));
-        _ = harmony.Patch(AccessTools.Method(typeof(PropertyBasedTooltipVM), nameof(PropertyBasedTooltipVM.Tick)), finalizer: tick);
-        HarmonyMethod show = new(typeof(Sorting), nameof(Sorting.ShowTooltip));
-        _ = harmony.Patch(AccessTools.Method(typeof(PropertyBasedTooltipVM), nameof(PropertyBasedTooltipVM.OnShowTooltip)), finalizer: show);
-        HarmonyMethod begin = new(typeof(Sorting), nameof(Sorting.BeginTooltip));
-        _ = harmony.Patch(AccessTools.Method(typeof(BasicTooltipViewModel), nameof(BasicTooltipViewModel.ExecuteBeginHint)), finalizer: begin);
-        HarmonyMethod add = new(typeof(Sorting), nameof(Sorting.AddTooltip));
-        _ = harmony.Patch(AccessTools.Method(typeof(ExplainedNumber), nameof(ExplainedNumber.Add)), add);
-        if (ModuleHelper.GetModuleInfo("Native").Version < ApplicationVersion.FromString("v1.1.0"))
-            return;
-        HarmonyMethod include = new(typeof(Sorting), nameof(Sorting.IncludeDetails));
-        _ = harmony.Patch(AccessTools.Method(typeof(DefaultClanFinanceModel), nameof(DefaultClanFinanceModel.CalculateClanGoldChange)), include);
-        _ = harmony.Patch(AccessTools.Method(typeof(DefaultClanFinanceModel), nameof(DefaultClanFinanceModel.CalculateClanIncome)), include);
-        _ = harmony.Patch(AccessTools.Method(typeof(DefaultClanFinanceModel), nameof(DefaultClanFinanceModel.CalculateClanExpenses)), include);
     }
 }
